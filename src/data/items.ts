765377764563eb63f1e5aa649e94d1dbb6e72316
Renan Piovesani <renan.piovesani2021@gmail.com>
2023-01-28 11:14:42 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items : Item[]= [
+    { date: new Date(2023, 0, 28), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2023, 0, 28), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2023, 0, 28), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2023, 0, 31), category: 'salary', title: 'Salário', value: 2000 },
+];
